bf6e593e374e79017b094c0116ea95933e09933a
Danny Blonquist <danny@devpipeline.com>
2023-09-07 07:28:38 -0700

    added contact page refresh on submit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, createContext } from "react";
+import { useState, createContext } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import NavBar from "./components/nav/NavBar";
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,18 +1,61 @@
+import { useState } from "react";
+
 import "../../styles/contact.scss";
 
 const Contact = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    if (name === "firstName") {
+      setFirstName(value);
+    } else if (name === "lastName") {
+      setLastName(value);
+    } else if (name === "email") {
+      setEmail(value);
+    } else if (name === "message") {
+      setMessage(value);
+    }
+  };
+
+  const handleClear = () => {
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setMessage("");
+  };
+
   return (
     <div className="contact-page">
       <h1>Contact Us!</h1>
-      <label>First</label>
-      <input type="text" />
-      <label>Last</label>
-      <input type="text" />
-      <label>Email</label>
-      <input type="text" placeholder="someone@example.com" />
+      <label>First:</label>
+      <input
+        type="text"
+        name="firstName"
+        value={firstName}
+        onChange={handleInput}
+      />
+      <label>Last:</label>
+      <input
+        type="text"
+        name="lastName"
+        value={lastName}
+        onChange={handleInput}
+      />
+      <label>Email:</label>
+      <input
+        type="text"
+        name="email"
+        value={email}
+        onChange={handleInput}
+        placeholder="someone@example.com"
+      />
       <label>Message:</label>
-      <textarea />
-      <button>Submit</button>
+      <textarea name="message" value={message} onChange={handleInput} />
+      <button onClick={handleClear}>Submit</button>
     </div>
   );
 };
